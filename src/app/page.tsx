c7db3191962089da8e5bfed56f77e9a9ecf6d7d5
Sai Kiran (Mike) Jattu <sjattu@ualberta.ca>
2025-09-26 23:44:53 -0600

    added get in touch info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -236,10 +236,9 @@ export default function Page() {
                   href={DATA.contact.social.X.url}
                   className="text-blue-500 hover:underline"
                 >
-                  with a direct question on twitter
-                </Link>{" "}
-                and I&apos;ll respond whenever I can. I will ignore all
-                soliciting.
+                  with a direct question on LinkedIn
+                </Link>{"https://www.linkedin.com/in/mikejattu/"}
+                and I&apos;ll respond whenever I can. 
               </p>
             </div>
           </BlurFade>
